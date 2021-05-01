04105caefa7820741cad857932559c2124228ec8
veryprairie <80795552+veryprairie@users.noreply.github.com>
2021-04-30 19:24:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/users", (req, res) => {
 
 
 // localhost:3003
-app.listen(3003, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log("Server is up and listening on 3003")
 });
 
